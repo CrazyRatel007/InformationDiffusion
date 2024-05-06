4ae3a3bd2fd850e07abdbd2eec05ee5a3b5a9e50
CrazyRatel007 <jc.jiang@siat.ac.cn>
2024-05-06 11:12:29 +0800

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,13 @@
+﻿// MultiLayerNetworkAnalysis.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "InformationDiffusion.h"
+
+
+
+int main(int argc, char* argv[])
+{
+	InformationDiffusion tmpTest;
+	return tmpTest.OnlineOfflineInformationDiffusion(argc, argv);
+}
